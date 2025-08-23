41121dbda5978c5e18142314469dbbe3fce6360c
ayushhmaan <2023ci_ayushmanpathak_a@gmail.com>
2025-08-23 20:15:51 +0530

    Initial commit for CineMate project

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { ArrowRight, CalendarIcon, ClockIcon } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+
+const HeroSection = () => {
+
+  const navigate = useNavigate()
+  return (
+    <div
+      className="flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 
+      bg-[url('/backgroundImage.png')] bg-cover bg-center h-screen"
+    >
+      <img src = {assets.marvelLogo} alt = "" className='max-h-11 lg:h-11 mt-20'/>
+
+      <h1 className='text-5x1 md:text-[70px] md:leading-18 font-semibold max-w-110'>
+        Guardians <br/> of the Galaxy
+      </h1>
+
+      <div className='flext items-center gap-4 text-gray-300'>
+        <span>Action | Adventure | Sci-Fi</span>
+        <div className='flex items-centre gap-1'>
+          <CalendarIcon className='w-4.5 h-4.5'/> 2018
+        </div>
+        <div className='flex items-centre gap-1'>
+          <ClockIcon className='w-4.5 h-4.5'/> 2h 8m
+        </div>
+        <p className='max-w-md text-gray-300'>Guardians of the Galaxy is a Marvel Studios film that follows a group of SuperHeros who team up to protect the galaxy. Blending action, humor, and heart, the movie delivers an adventurous space journey filled with stunning visuals, unforgettable characters.
+        </p>
+        <button onClick={() => navigate('/movies')} className='flex items-centre gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transaction rounded-full font-medium cursor-pointer'>
+          Explore Movies
+          <ArrowRight className='w-5 h-5'/>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeatureSection from '../components/FeatureSection'
+
+const home = () => {
+  return (
+    <>
+      <HeroSection/>
+      <FeatureSection/>
+    </>
+  )
+}
+
+export default home
